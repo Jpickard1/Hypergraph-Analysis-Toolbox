9efa893ce8b652f6e35a1657fedea465878c564f
jpickard1 <jpic@umich.edu>
2022-10-04 06:04:49 -0400

    missed multilinealg

--- Python/HAT/multilinalg.py
+++ Python/HAT/multilinalg.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy as sp
 
-def hosvd(T, M=True uniform=False, sym=False):
+def hosvd(T, M=True, uniform=False, sym=False):
     """ Higher Order Singular Value Decomposition
     
     :param uniform: Indicates if T is a uniform tensor
@@ -11,7 +11,7 @@ def hosvd(T, M=True uniform=False, sym=False):
     :return: The singular values of the core diagonal tensor and the factor matrices.
     
     """
-    if uniform and !M:
+    if uniform and not M:
         return uniformSymHosvd(T)
     else:
         print('Nonuniform SVD not implemented')
