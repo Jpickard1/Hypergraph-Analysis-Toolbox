9dc9777d8d94245d99f23e0b4e3776add486502f
jpickard1 <jpic@umich.edu>
2022-11-15 14:34:45 -0500

    Removed old code and created Matlab HAT package

--- Matlab/+DissimilarityMeasures/+TensorDis/Hamming.m
+++
@@ -1,9 +0,0 @@
-function d = Hamming(A1, A2)
-% A1 and A2 are adjacency tensors of two hypergraphs
-% Returns the Hamming distance between two hypergraphs
-
-sz = size(A1);
-order = length(sz);
-d = sum(abs(A1-A2), 'all')/(sz(1)^order-sz(1));
-
-end
