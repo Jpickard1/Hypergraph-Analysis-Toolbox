1d1d01160c5a287995ba77c591bcd3b8672094f3
JPickard1 <jpic@umich.edu>
2022-12-01 14:09:07 -0500

    DOCS: conf.py mod networkx error in RTD build logs

--- Python/docs/conf.py
+++ Python/docs/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.linalg']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.linalg', 'networkx']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
