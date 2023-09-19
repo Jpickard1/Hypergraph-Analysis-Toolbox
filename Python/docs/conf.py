f71d08811677312dd52f35c7cbe2663da4069b77
JPickard1 <jpic@umich.edu>
2023-09-18 22:43:09 -0400

    added moc module to fix autodoc issue for HAT documentation page

--- Python/docs/conf.py
+++ Python/docs/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.linalg', 'networkx']
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.io', 'matplotlib', 'matplotlib.pyplot', 'scipy.linalg', 'networkx']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
